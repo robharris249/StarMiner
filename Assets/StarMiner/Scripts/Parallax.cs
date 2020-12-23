0a6c3c7f69d9db2cb31878fb89d96ba272e06bb3
Rob <robharris249@gmail.com>
2020-12-23 01:11:10 +0000

    Added Parallax effect

---
+++ Assets/StarMiner/Scripts/Parallax.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    private float length, height, startPosX, startPosY;
+    public GameObject cam;
+    public float parallaxStrength;
+
+    // Start is called before the first frame update
+    void Start() {
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float tempX = (cam.transform.position.x * (1 - parallaxStrength));
+        float tempY = (cam.transform.position.y * (1 - parallaxStrength));
+        float distX = (cam.transform.position.x * parallaxStrength);
+        float distY = (cam.transform.position.y * parallaxStrength);
+
+        transform.position = new Vector3(startPosX + distX, startPosY + distY, transform.position.z);
+
+        if (tempX > startPosX + length) startPosX += length;
+        else if (tempX < startPosX - length) startPosX -= length;
+
+        if (tempY > startPosY + height) startPosY += height;
+        else if (tempY < startPosY - height) startPosY -= height;
+    }
+}
